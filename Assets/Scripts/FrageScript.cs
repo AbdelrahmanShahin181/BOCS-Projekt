bede2872865ca0de186dc990ad945c091426d139
AbdelrahmanShahin181 <abdelrahman.shahin@stud.hs-bochum.de>
2022-06-14 22:05:56 +0200

    Abdo: Fragemaske

--- Assets/Scripts/FrageScript.cs
+++ Assets/Scripts/FrageScript.cs
@@ -7,21 +7,21 @@ using TMPro;
 public class FrageScript : MonoBehaviour
 {
 
-    public TextMeshPro frage;
-    public TextMeshPro antwort1;
-    public TextMeshPro antwort2;
-    public TextMeshPro antwort3;
-    public TextMeshPro antwort4;
+    public TextMeshProUGUI frage;
+    public TextMeshProUGUI antwort1;
+    public TextMeshProUGUI antwort2;
+    public TextMeshProUGUI antwort3;
+    public TextMeshProUGUI antwort4;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        frage.text =  "Hallo Ich bin Abdo ";
-        antwort1.text= "A";
-        antwort2.text= "B";
-        antwort3.text= "C";
-        antwort4.text= "D";
+        frage.text =  "System.out.print...()";
+        antwort1.text= "line";
+        antwort2.text= "lime";
+        antwort3.text= "life";
+        antwort4.text= "like";
     }
 
     public void printhallo (){
