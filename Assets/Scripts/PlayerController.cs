9250869bec97c7c43eac369b6d585758e1920397
Jalal Alyousef <m.jalal.alyousef@gmail.com>
2022-10-16 17:01:59 +0200

    playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public Rigidbody2D player;
-    [SerializeField] private float speed;
+  public float speed ;
+    private Rigidbody2D myRigidbody ;
+    private Vector3 change;
+    private Animator animator ;
     // Start is called before the first frame update
     void Start()
     {
-        player= GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        player.velocity= player.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero){
+            MoveCharacter();
+            animator.SetFloat("MoveX",change.x);
+            animator.SetFloat("MoveY",change.y);
+            animator.SetBool("moving",true);
+
+
+        }  else{
+            animator.SetBool("moving",false);
+        }      
+    }
+    void MoveCharacter(){
+        myRigidbody.MovePosition(transform.position+ change*speed* Time.deltaTime);
     }
 }
