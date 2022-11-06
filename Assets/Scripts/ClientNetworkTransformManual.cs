1b04d2385663844dc15d16f07f10182ad66d5639
AbdelrahmanShahin <91955819+AbdelrahmanShahin181@users.noreply.github.com>
2022-11-06 15:34:19 +0100

    minor update

--- Assets/Scripts/ClientNetworkTransformManual.cs
+++ Assets/Scripts/ClientNetworkTransformManual.cs
@@ -5,66 +5,17 @@ using Unity.Netcode;
 
 public class ClientNetworkTransformManual : NetworkBehaviour
 {
-    [SerializeField] private bool _serverAuth;
-    [SerializeField] private float _cheapInterpolationTime = 0.1f;
+    private NetworkVariable<Vector3> _netPos = new (writePerm: NetworkVariableWritePermission.Owner);
 
-    private NetworkVariable<PlayerNetworkState> _playerState;
-    private Rigidbody _rb;
-
-    private void Awake() {
-        _rb = GetComponent<Rigidbody>();
-        var permission = _serverAuth ? NetworkVariableWritePermission.Server : NetworkVariableWritePermission.Owner;
-        _playerState = new NetworkVariable<PlayerNetworkState>(writePerm: permission);
-    }
-
-    public override void OnNetworkSpawn() {
-        if (!IsOwner) Destroy(transform.GetComponent<PlayerController>());
-    }
-
-    private void Update() {
-        if (IsOwner) TransmitState();
-        else ConsumeState();
-    }
-
-
-    private void TransmitState() {
-        var state = new PlayerNetworkState {
-            Position = _rb.position,
-        };
-
-        if (IsServer || !_serverAuth)
-            _playerState.Value = state;
-        else
-            TransmitStateServerRpc(state);
-    }
-
-    [ServerRpc]
-    private void TransmitStateServerRpc(PlayerNetworkState state) {
-        _playerState.Value = state;
-    }
-
-
-    private Vector3 _posVel;
-
-    private void ConsumeState() {
-        // Here you'll find the cheapest, dirtiest interpolation you'll ever come across. Please do better in your game
-        _rb.MovePosition(Vector3.SmoothDamp(_rb.position, _playerState.Value.Position, ref _posVel, _cheapInterpolationTime));
-    }
-
-    private struct PlayerNetworkState : INetworkSerializable {
-        private float _posX, _posY;
-
-        internal Vector3 Position {
-            get => new(_posX, 0, _posY);
-            set {
-                _posX = value.x;
-                _posY = value.y;
-            }
+    void Update()
+    {
+        if (IsOwner)
+        {
+            _netPos.Value = transform.position;
         }
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
-            serializer.SerializeValue(ref _posX);
-            serializer.SerializeValue(ref _posY);
+        else
+        {
+            transform.position = _netPos.Value;
         }
     }
 }
