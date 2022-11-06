a20fa38d222da00e64ad22091a09287d9378e874
AbdelrahmanShahin <91955819+AbdelrahmanShahin181@users.noreply.github.com>
2022-11-06 15:55:18 +0100

    minor update

--- Assets/Scripts/ClientNetworkTransformManual.cs
+++ Assets/Scripts/ClientNetworkTransformManual.cs
@@ -5,36 +5,19 @@ using Unity.Netcode;
 
 public class ClientNetworkTransformManual : NetworkBehaviour
 {
-    private NetworkVariable<PlayerNetworkState> _netState = new (writePerm: NetworkVariableWritePermission.Owner);
+    private NetworkVariable<Vector3> _netPos = new (writePerm: NetworkVariableWritePermission.Owner);
 
     void Update()
     {
         if (IsOwner)
         {
-            _netState.Value = new PlayerNetworkState();
+            _netPos.Value = transform.position;
         }
         else
         {
-            transform.position = _netState.Value.Position;
+            transform.position = _netPos.Value;
         }
     }
-
-    private struct PlayerNetworkState : INetworkSerializable {
-        private float _posX, _posY;
-
-        internal Vector3 Position {
-            get => new(_posX, 0, _posY);
-            set {
-                _posX = value.x;
-                _posY = value.y;
-            }
-        }
-
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
-            serializer.SerializeValue(ref _posX);
-            serializer.SerializeValue(ref _posY);
-        }
-    }
+    
 }
 
