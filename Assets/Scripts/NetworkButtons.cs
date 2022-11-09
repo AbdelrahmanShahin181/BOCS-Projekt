137975275f35090880d4bfbbc3167e8c75e9e2ed
AbdelrahmanShahin <91955819+AbdelrahmanShahin181@users.noreply.github.com>
2022-11-09 12:48:07 +0100

    networking minor updates

--- Assets/Scripts/NetworkButtons.cs
+++ Assets/Scripts/NetworkButtons.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using System;
+
 
 public class NetworkButtons : MonoBehaviour {
+    private void Awake(){
+        //new UnityLogger();
+        print("Call Awake");
+        Application.targetFrameRate = 30;
+        string [] args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++){
+            if (args[i] == "-server"){
+                NetworkManager.Singleton.StartServer();
+            }
+            else if (args[i] == "-client"){
+                NetworkManager.Singleton.StartClient();
+            }
+            else if (args[i] == "-host"){
+                NetworkManager.Singleton.StartHost();
+            }
+        }
+        //NetworkManager.Singleton.StartClient();
+    }
+
     private void OnGUI() {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
