60d5abf49f5e434bd718e1681115e172cd84fbdb
RobinKoehne <robin@koehne.com>
2022-11-15 10:46:14 +0100

    Healing Coffee & Dialog Box

--- Assets/Scripts/KaffeeautomatDialog.cs
+++ Assets/Scripts/KaffeeautomatDialog.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class KaffeeautomatDialog : MonoBehaviour
 {
-    public GameObject Dialog;
-    public GameObject DialogText;
+    public GameObject dialogBox;
+    public Text dialogText;
     public string dialog;
-    public bool isDialogActive = false;
+    public bool playerInRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +18,36 @@ public class KaffeeautomatDialog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
     }
     
     public void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Player_Boy" || other.gameObject.tag == "Player_Girl") {
-            isDialogActive = true;
-            Dialog.SetActive(true);
-            DialogText.GetComponent<Text>().text = dialog;
+        if (other.CompareTag("Player")) {
+            Debug.Log("Player entered");
+            playerInRange = true;
+
+            if(Input.GetKeyDown(KeyCode.J)){
+                var health = other.GetComponent<HealthManager>();
+                health.heal(5);
+            }
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Player left");
+            playerInRange = false;
+            dialogBox.SetActive(false);
         }
     }
 }
