dff0cad98e64768ac56933d5ebce26c761680b7d
AbdelrahmanShahin <91955819+AbdelrahmanShahin181@users.noreply.github.com>
2022-11-17 15:30:57 +0100

    Minor Updates

---
+++ Assets/Scripts/Netcode/ChatMessage.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+
+public class ChatMessage : MonoBehaviour {
+    [SerializeField] private TMP_Text textField;
+
+    public void SetMessage(string playerName, string message) {
+        textField.text = $"<color=grey>{playerName}</color>: {message}";
+    }
+}
