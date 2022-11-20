a0180c5505443f7d6a3234e4d67fc7da0fa193fb
MalteReth <91974898+MalteReth@users.noreply.github.com>
2022-11-20 16:28:40 +0100

    Karte und Navigation weitergebaut

---
+++ Assets/Scripts/Navigation/ChangeLayerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Key {W,A,S,D};
+
+public class ChangeLayerScript : MonoBehaviour
+{
+    public Key up = Key.W;
+    public Key down = Key.S;
+    public int upFloorNum = 1;
+    [SerializeField] GameObject _lowerLevel;
+    [SerializeField] GameObject _upperLevel;    
+
+    KeyCode KeyToKeyCode(Key key) {
+        switch(key) {
+            case Key.W: return KeyCode.W;
+            case Key.A: return KeyCode.A;
+            case Key.S: return KeyCode.S;
+            case Key.D: return KeyCode.D;
+        }
+        return KeyCode.W;
+    }
+
+
+    void OnTriggerExit2D(Collider2D pl) {
+        if(pl.tag == "Player") {
+        Debug.Log("Trigger");
+            if(Input.GetKey(KeyToKeyCode(up))) {
+                _upperLevel.transform.GetChild(2).gameObject.SetActive(true);
+                _upperLevel.transform.GetChild(1).gameObject.SetActive(true);
+                _lowerLevel.transform.GetChild(1).gameObject.SetActive(false);
+                _lowerLevel.transform.GetChild(0).gameObject.SetActive(false);
+                _upperLevel.transform.GetChild(0).gameObject.SetActive(true);
+    
+                pl.GetComponent<SpriteRenderer>().sortingLayerName = upFloorNum + "_Def";
+            }
+            if(Input.GetKey(KeyToKeyCode(down))) {
+                _upperLevel.transform.GetChild(1).gameObject.SetActive(false);
+                _upperLevel.transform.GetChild(2).gameObject.SetActive(false);
+                _lowerLevel.transform.GetChild(1).gameObject.SetActive(true);
+                _lowerLevel.transform.GetChild(0).gameObject.SetActive(true);
+                _upperLevel.transform.GetChild(0).gameObject.SetActive(false);
+                
+                pl.GetComponent<SpriteRenderer>().sortingLayerName = (upFloorNum-1) +"_Def";
+            }
+        }
+    }
+}
