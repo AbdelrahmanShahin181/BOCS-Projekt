75787e3ec9164fc0c7c7bb3b6d8aecfe775cae71
RobinKoehne <robin@koehne.com>
2022-12-06 14:58:47 +0100

    ECTS-Währung

---
+++ Assets/Scripts/ECTSPickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ECTSPickup : MonoBehaviour
+{
+    public int value;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            other.gameObject.GetComponent<ECTSCounter>().erhoereWert(value);
+            Destroy(gameObject);
+        }
+        
+    }
+}
