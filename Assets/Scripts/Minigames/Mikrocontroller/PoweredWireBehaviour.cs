df15309580b7978a4f44fef4474284c68dc6966a
DavidDors1 <92305487+DavidDors1@users.noreply.github.com>
2022-12-10 09:42:46 +0100

    Mikrokontroller Minispiel

---
+++ Assets/Scripts/Minigames/Mikrocontroller/PoweredWireBehaviour.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredWireBehaviour : MonoBehaviour
+{
+    bool mouseDown = false;
+    public PoweredWireStats powerWireS;
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerWireS = gameObject.GetComponent<PoweredWireStats>();
+        line = gameObject.GetComponentInParent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWire();
+        
+        line.SetPosition(3, new Vector3(gameObject.transform.localPosition.x-.1f,gameObject.transform.localPosition.y-.1f,0));
+        line.SetPosition(2, new Vector3(gameObject.transform.localPosition.x-.4f,gameObject.transform.localPosition.y-.1f,0));
+    }
+
+    void OnMouseDown()
+    {
+        mouseDown = true;
+    }
+
+    void OnMouseOver()
+    {
+        powerWireS.movable = true;
+    }
+
+    void OnMouseExit()
+    {
+        if(!powerWireS.moving)
+        {
+            powerWireS.movable = false;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        mouseDown = false;
+        gameObject.transform.position = powerWireS.startPosition;
+    }
+
+    void MoveWire()
+    {
+        if(mouseDown && powerWireS.movable)
+        {
+            powerWireS.moving = true;
+            float mouseX = Input.mousePosition.x;
+            float mouseY = Input.mousePosition.y;
+
+            gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 1));
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, transform.parent.transform.position.z);
+        }
+        else
+        {
+            powerWireS.moving = false;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Minigames/Mikrocontroller/PoweredWireStats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Color { black, red, yellow, white };
+
+public class PoweredWireStats : MonoBehaviour
+{
+    public bool movable = false;
+    public bool moving = false;
+    public Vector3 startPosition;
+    public Color objectColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
