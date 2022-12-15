0582d4de6e930075d451e95aed60aba93f1f122b
RobinKoehne <robin@koehne.com>
2022-12-15 10:27:17 +0100

    NPC-Changes
    NPC-Movement individuell einstellbar
    NPC_Quizmechanik implementiert

---
+++ Assets/Dialogs/Choices_NPC_2.ink
@@ -0,0 +1,16 @@
+﻿-> main
+
+=== main ===
+Wie kann man in der Mensa bezahlen?
+	+ [Ausschließlich Bar]
+		-> antwort("Bar")
+	+ [Die Verpflegungskosten sind im Semesterbeitrag unter >Verpflegung< enthalten]
+		-> antwort("Semesterbeitrag")
+	+ [Mit der EC-Karte oder dem Studierendenausweis]
+		-> antwort("Karte")
+	+ [Das Essen ist kostenlos]
+		-> antwort("Kostenlos")
+
+=== antwort(auswahl)
+Du hast {auswahl} gewählt!
+-> END
---
+++ Assets/Scripts/NPCQuizManager.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+
+public class NPCQuizManager : MonoBehaviour
+{
+    [Header("Dialog UI")]
+    [SerializeField] private GameObject dialogPanel;
+    [SerializeField] private TextMeshProUGUI dialogText;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choiceTexts;
+    private Story currentStory;
+    public bool dialogIsActive{get; private set;}
+
+    private static NPCQuizManager instance;
+
+    ECTSCounter ectsCounter;
+    GameObject playerBoy;
+
+    private void Awake(){
+
+        if(instance != null){
+
+            Debug.LogWarning("Es gibt mehrere Dialog Manager in der Scene!");
+        }
+
+            instance = this;
+    }
+
+    public static NPCQuizManager GetInstance(){
+
+        if(instance == null){
+
+            instance = new NPCQuizManager();
+        }
+
+        return instance;
+    }
+
+    private void Start(){
+
+        dialogIsActive = false;
+        dialogPanel.SetActive(false);
+
+        choiceTexts = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices){
+
+            choiceTexts[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    private void Update(){
+
+        if(dialogIsActive){
+
+            if(Input.GetKeyDown(KeyCode.E)){
+
+                ContinueStory();
+            }
+        }
+    }
+
+    public void StartDialog(TextAsset inkJSON){
+
+        currentStory = new Story(inkJSON.text);
+        dialogIsActive = true;
+        dialogPanel.SetActive(true);
+        ContinueStory();
+    }
+
+    private void ExitDialogeMode(){
+
+        dialogIsActive = false;
+        dialogPanel.SetActive(false);
+        dialogText.text = "";
+    }
+
+    private void ContinueStory(){
+
+        if(currentStory.canContinue){
+
+            // Setze DialogText auf den nächsten Knoten
+            dialogText.text = currentStory.Continue();
+            // Zeige die Choices an
+            DisplayChoices();
+        }
+
+        else{
+
+            ExitDialogeMode();
+        }
+    }
+
+    private void DisplayChoices(){
+
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        //Check if there are more choices than UI elements
+        if(currentChoices.Count > choices.Length){
+
+            Debug.LogWarning("Es gibt mehr Choices als UI Elemente! Es werden nur: " + currentChoices.Count + " angezeigt!");
+        }
+
+        int index = 0;
+
+        //Display all choices
+        foreach(Choice choice in currentChoices){
+
+            choices[index].SetActive(true);
+            choiceTexts[index].text = choice.text;
+            index++;
+        }
+
+        // Hide all unused choices
+        for(int i = index; i < choices.Length; i++){
+
+            choices[i].SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice(){
+        //Das Eventsystem muss auf null gesetzt werden, damit es nicht mehr auf das letzte UI Element zeigt
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void ChooseChoice(int choiceIndex){
+
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
+
+        if(choiceIndex == 2){
+
+            AddECTS();
+        }
+
+    }
+
+    public void AddECTS(){
+
+        ectsCounter = GameObject.Find("Player_Boy").GetComponent<ECTSCounter>();
+        ectsCounter.erhoeheWert(5);
+    }
+
+
+}
